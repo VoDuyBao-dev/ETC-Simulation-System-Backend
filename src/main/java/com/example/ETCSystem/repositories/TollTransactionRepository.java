b5ffc97a7f11bbbc36f1c43602110ba6c16b1d09
VoDuyBao-dev <voduybao19052005@gmail.com>
2025-11-20 16:17:28 +0700

    edit get toll transaction for admin

--- src/main/java/com/example/ETCSystem/repositories/TollTransactionRepository.java
+++ src/main/java/com/example/ETCSystem/repositories/TollTransactionRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TollTransactionRepository extends JpaRepository<TollTransaction,Long> {
 
-    Page<TollTransactionProjection> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    List<TollTransactionProjection> findAllByOrderByCreatedAtDesc();
 
 }
